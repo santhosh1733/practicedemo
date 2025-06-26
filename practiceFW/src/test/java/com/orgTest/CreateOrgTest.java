5e3cb60cab7cf9d30c0f1b71b00ea71dac1d4500
santhosh <santhosh@MSI>
2025-06-26 10:33:16 +0530

    ADD

--- practiceFW/src/test/java/com/orgTest/CreateOrgTest.java
+++ practiceFW/src/test/java/com/orgTest/CreateOrgTest.java
@@ -8,12 +8,12 @@ import org.testng.annotations.Test;
 
 public class CreateOrgTest {
 	@Test
-public void createOrg() {
-	System.out.println("createorganizaion");
-	System.out.println(System.getProperty("url"));
-	System.out.println(System.getProperty("browser"));
-	System.out.println(System.getProperty("username"));
-	System.out.println(System.getProperty("password"));
-	WebDriver driver=new EdgeDriver();
-}
+	public void createOrg() {
+		System.out.println("createorganizaion");
+		System.out.println(System.getProperty("url"));
+		System.out.println(System.getProperty("browser"));
+		System.out.println(System.getProperty("username"));
+		System.out.println(System.getProperty("password"));
+		WebDriver driver = new EdgeDriver();
+	}
 }
