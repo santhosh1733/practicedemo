99367488158778b0c13d6533b92559b68796b668
santhosh <santhosh@MSI>
2025-06-26 10:16:31 +0530

    browser changed

--- practiceFW/src/test/java/com/orgTest/CreateOrgTest.java
+++ practiceFW/src/test/java/com/orgTest/CreateOrgTest.java
@@ -2,6 +2,7 @@ package com.orgTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class CreateOrgTest {
@@ -12,6 +13,6 @@ public void createOrg() {
 	System.out.println(System.getProperty("browser"));
 	System.out.println(System.getProperty("username"));
 	System.out.println(System.getProperty("password"));
-	WebDriver driver=new ChromeDriver();
+	WebDriver driver=new FirefoxDriver();
 }
 }
