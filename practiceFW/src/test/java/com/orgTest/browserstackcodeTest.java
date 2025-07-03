25764364bc304956a76e0eb33eb1b7c37ab832b0
santhosh <santhosh@MSI>
2025-07-03 22:20:47 +0530

    m

---
+++ practiceFW/src/test/java/com/orgTest/browserstackcodeTest.java
@@ -0,0 +1,36 @@
+package com.orgTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class browserstackcodeTest {
+@Test
+public void sampletest() throws MalformedURLException, InterruptedException {
+	MutableCapabilities capabilities = new MutableCapabilities();
+	HashMap<String, Object> bstackOptions = new HashMap<String, Object>();
+	capabilities.setCapability("browserName", "Chrome");
+	bstackOptions.put("os", "Windows");
+	bstackOptions.put("osVersion", "10");
+	bstackOptions.put("browserVersion", "120.0");
+	bstackOptions.put("consoleLogs", "info");
+	capabilities.setCapability("bstack:options", bstackOptions);
+
+	String username="santhoshnagaraj_UHAQjl";
+	String password="9ms7ynKvxSP8qA6grxGw";
+	RemoteWebDriver driver=new RemoteWebDriver(new URL("https://"+username+":"+password+"@hub-cloud.browserstack.com/wd/hub"), capabilities);
+	Thread.sleep(3000);
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	driver.get("https://in.bookmyshow.com/explore/home/bengaluru");
+	//driver.findElement(By.xpath("(//*[local-name()='svg']/*[name()='path'])[2]")).click();
+    driver.findElement(By.xpath("//span[text()='Bengaluru']")).click();
+    driver.findElement(By.xpath("//span[text()='Bengaluru']/..//div")).click();
+}
+}
